a9134c9504a3638fe1f6e41b68c0d4cdd381e6a5
Sanya <sanya@sanyaiv.com>
2020-04-06 17:25:57 +0200

    Fixed up tests

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,7 +1,8 @@
 #include "pch.h"
 #include "../CalculatorStaticLibrary/Calculator.h"
 
-TEST(Calculator, HandlesOutOfBounds) {
+TEST(Calculator, HandlesOutOfBounds)
+{
     ASSERT_THROW(Calculator::CalculateLevelCost(0, 31), std::runtime_error);
 }
 
