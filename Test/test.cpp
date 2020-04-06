974995e6c02a55f2ee1bc24c9170e9f76cd03e24
Sanya <sanya@sanyaiv.com>
2020-04-06 17:13:23 +0200

    Update test.cpp

--- Test/test.cpp
+++ Test/test.cpp
@@ -4,3 +4,27 @@
 TEST(Calculator, HandlesOutOfBounds) {
     ASSERT_THROW(Calculator::CalculateLevelCost(0, 31), std::runtime_error);
 }
+
+TEST(Calculator, HandlesIncorrectOrder)
+{
+    ASSERT_THROW(Calculator::CalculateLevelCost(10, 9), std::runtime_error);
+}
+
+TEST(Calculator, HandlesSameLevel)
+{
+    ASSERT_THROW(Calculator::CalculateLevelCost(10, 10), std::runtime_error);
+}
+
+TEST(Calculator, HandlesCalculatingLevel0To1)
+{
+    auto Cost = Calculator::CalculateLevelCost(0, 1);
+    EXPECT_EQ(Cost.first, 10);
+    EXPECT_EQ(Cost.second, 5);
+}
+
+TEST(Calculator, HandlesCalculatingLevel0To30)
+{
+    auto Cost = Calculator::CalculateLevelCost(0, 30);
+    EXPECT_EQ(Cost.first, 11570);
+    EXPECT_EQ(Cost.second, 5165);
+}
